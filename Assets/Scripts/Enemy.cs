62953951c60958d409bd50110a9539fffbdbcf68
RullerBlate <115470262+RullerBlate@users.noreply.github.com>
2022-10-28 17:18:42 +0300

    Summary

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private bool _isGoingRight = true;
+
+    private void Update()
+    {
+        if (_isGoingRight)
+        transform.Translate(1 * Time.deltaTime, 0, 0);
+        else
+        transform.Translate(-1 * Time.deltaTime, 0, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("RightWall"))
+            _isGoingRight = false;
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("LeftWall"))
+            _isGoingRight = true;
+    }
+}
